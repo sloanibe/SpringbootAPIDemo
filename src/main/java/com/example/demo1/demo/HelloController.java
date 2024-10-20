43182fdc712802c8427073175f40628142672b28
sloanibe <sloanibe@gmail.com>
2024-10-19 17:16:26 -0700

    222 msg

--- src/main/java/com/example/demo1/demo/HelloController.java
+++ src/main/java/com/example/demo1/demo/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hello, Spring Boot 111!";
+        return "Hello, Spring Boot 222!";
     }
 
 }
